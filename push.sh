e3c643ab7394d7b3b4fb427fe9307ae31f427138
sudheer_muthyala <34484660+sudheer919@users.noreply.github.com>
2021-03-28 20:15:20 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -2,8 +2,24 @@
 #ls -l
 #git status
 
-cd $(ls -lt | awk 'NR==2{print$NF}')
+#cd $(ls -lt | awk 'NR==2{print$NF}')
+
+#git add .
+#git commit -m"auto pushing chainges repo"
+#git push 
+
+
+#######################################################################################
+#pwd
+#ls -l
+#git status
+
+#cd $(ls -lt | awk 'NR==2{print$NF}')
+cd $(stat cd $(ls -lt | awk 'NR==2{print$NF}') | awk ' NR==1 {print $2}')
 
 git add .
 git commit -m"auto pushing chainges repo"
-git push 
+git push
+
+#######################################################################################
+# stat is a command is to state resent chainges of files and Directory 
